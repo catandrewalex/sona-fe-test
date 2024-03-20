17a7ecb8fd09cff7441cf48803dac61a9d68f58c
FerdiantJoshua <ferdiant.98@gmail.com>
2024-03-20 08:21:21 +0700

    Update URL path for /presence/classId

--- api/modules/presence.ts
+++ api/modules/presence.ts
@@ -10,7 +10,7 @@ const GetPresenceByClass = (
   { page = 1, resultsPerPage = 10000, classId }: GetPresenceRequestConfig = { classId: 0 }
 ): Promise<FailedResponse | SuccessResponse<Presence>> => {
   return API.get<Presence>({
-    url: `${Routes.PRESENCE}/${classId}`,
+    url: `${Routes.CLASS}/${classId}/presences`,
     config: { params: { page, resultsPerPage } }
   });
 };
