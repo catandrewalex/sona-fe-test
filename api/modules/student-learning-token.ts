a808917fac3e11e5f8c236f7b3ca52dcbb74763c
FerdiantJoshua <ferdiant.98@gmail.com>
2025-01-22 19:09:40 +0700

    Add dummy URL query param to GetStudentLearningTokenDisplayByClass()

--- api/modules/student-learning-token.ts
+++ api/modules/student-learning-token.ts
@@ -5,7 +5,9 @@ const GetStudentLearningTokenDisplayByClass = (
   classId: number
 ): Promise<FailedResponse | SuccessResponse<StudentWithStudentLearningTokensDisplay>> => {
   return API.get<StudentWithStudentLearningTokensDisplay>({
-    url: `${Routes.CLASS}/${classId}/studentLearningTokensDisplay`
+    // we add dummy URL query parameter "no-cache", to prevent any intermediate server from caching this request,
+    // thus causing the FE unable to refresh the data, unless the cache has expired
+    url: `${Routes.CLASS}/${classId}/studentLearningTokensDisplay?no-cache=true`
   });
 };
 
